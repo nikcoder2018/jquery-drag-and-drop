fed22ef98b72b9b727cd30b7cca01b5ae7dcb855
nikcoder <b.nickjay05@gmail.com>
2020-01-15 15:18:43 +0800

    add all files

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>jQuery UI Draggable</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <style>
+
+        </style>
+    </head>
+    <body >
+
+        <div class="container" >
+            <input type="file" name="file" id="file">
+
+            <!-- Drag and Drop container-->
+            <div class="upload-area"  id="uploadfile">
+                <h1>Drag and Drop file here<br/>Or<br/>Click to select file</h1>
+            </div>
+        </div>
+        <script src="jquery.min.js" type="text/javascript"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script src="script.js" type="text/javascript"></script>
+        <script>
+          
+        </script>
+    </body>
+</html>
